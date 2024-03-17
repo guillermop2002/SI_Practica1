ed02b1e27142d666a2a7bdc54e09e58ac110a10b
guillermop2002 <guillermoprieto17@gmail.com>
2024-03-17 22:51:59 +0100

    Ejercicio 2 Entero

--- analisis_datos.py
+++ analisis_datos.py
@@ -1,48 +1,57 @@
 import sqlite3
 import pandas as pd
+import json
 
-conn = sqlite3.connect('etl_system.db')
+conn = sqlite3.connect('etl_database.db')
 
-df_usuarios = pd.read_sql_query("SELECT * FROM Usuarios", conn)
-df_emails = pd.read_sql_query("SELECT * FROM Emails", conn)
-df_fechas = pd.read_sql_query("SELECT username, fecha_cambio_contrasena FROM Fechas", conn)
-df_ips = pd.read_sql_query("SELECT username, ip FROM IPs", conn)
+df_legal_data = pd.read_sql_query("SELECT * FROM legal_data", conn)
+df_users_data = pd.read_sql_query("SELECT * FROM users_data", conn)
 
 conn.close()
 
-num_muestras = df_usuarios.dropna().shape[0]
+num_muestras = df_users_data.dropna().shape[0]
 print(f'Número de muestras: {num_muestras}')
 
-try:
-    df_fechas['fecha_cambio_contrasena'] = pd.to_datetime(df_fechas['fecha_cambio_contrasena'])
-except Exception as e:
-    print(f'Error al convertir fechas: {e}')
+df_users_data['fechas'] = df_users_data['fechas'].apply(json.loads)
+df_users_data['ips'] = df_users_data['ips'].apply(json.loads)
 
-df_fechas_por_usuario = df_fechas.groupby('username')['fecha_cambio_contrasena'].nunique()
-media_fechas = df_fechas_por_usuario.mean()
-desviacion_fechas = df_fechas_por_usuario.std()
+fechas_df = pd.DataFrame(
+    [
+        (username, pd.to_datetime(fecha, format='%d/%m/%Y', errors='coerce'))
+        for username, fechas in zip(df_users_data['username'], df_users_data['fechas'])
+        for fecha in fechas
+    ],
+    columns=['username', 'fecha_cambio_contrasena']
+)
+
+fechas_por_usuario = fechas_df.groupby('username')['fecha_cambio_contrasena'].nunique()
+media_fechas = fechas_por_usuario.mean()
+desviacion_fechas = fechas_por_usuario.std()
 print(f'Media de cambios de contraseña: {media_fechas}')
 print(f'Desviación estándar de cambios de contraseña: {desviacion_fechas}')
 
-df_ips_por_usuario = df_ips.groupby('username')['ip'].nunique()
-media_ips = df_ips_por_usuario.mean()
-desviacion_ips = df_ips_por_usuario.std()
+ips_df = pd.DataFrame([(username, ip)
+                       for username, ips in zip(df_users_data['username'], df_users_data['ips'])
+                       for ip in ips], columns=['username', 'ip'])
+
+ips_por_usuario = ips_df.groupby('username')['ip'].nunique()
+media_ips = ips_por_usuario.mean()
+desviacion_ips = ips_por_usuario.std()
 print(f'Media de IPs por usuario: {media_ips}')
 print(f'Desviación estándar de IPs por usuario: {desviacion_ips}')
 
-media_phishing = df_emails['phishing'].mean()
-desviacion_phishing = df_emails['phishing'].std()
+media_phishing = df_users_data['phishing_emails'].mean()
+desviacion_phishing = df_users_data['phishing_emails'].std()
 print(f'Media de emails de phishing: {media_phishing}')
 print(f'Desviación estándar de emails de phishing: {desviacion_phishing}')
 
-min_emails = df_emails['total'].min()
-max_emails = df_emails['total'].max()
+min_emails = df_users_data['total_emails'].min()
+max_emails = df_users_data['total_emails'].max()
 print(f'Número mínimo de emails recibidos: {min_emails}')
 print(f'Número máximo de emails recibidos: {max_emails}')
 
-admins = df_usuarios[df_usuarios['permisos'] == 1]
-admin_phishing = df_emails[df_emails['username'].isin(admins['username'])]
-min_phishing_admin = admin_phishing['phishing'].min()
-max_phishing_admin = admin_phishing['phishing'].max()
+admins = df_users_data[df_users_data['permisos'] == 1]
+min_phishing_admin = admins['phishing_emails'].min()
+max_phishing_admin = admins['phishing_emails'].max()
 print(f'Mínimo de emails de phishing interactuados por administradores: {min_phishing_admin}')
 print(f'Máximo de emails de phishing interactuados por administradores: {max_phishing_admin}')
